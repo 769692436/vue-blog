6391c53a5f944ae87129c32db3756c787f5b6134
KIng_Kel <769692436@qq.com>
2018-12-26 17:00:53 +0800

    新建项目

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="app-header layui-bg-black">
+    <div class="layui-container">
+      <ul class="layui-nav layui-bg-black">
+        <li class="layui-nav-item">
+          <a href="/">
+            <i class="layui-icon layui-icon-home"></i>
+            首页
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
